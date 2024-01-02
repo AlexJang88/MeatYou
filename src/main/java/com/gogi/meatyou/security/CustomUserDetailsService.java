31ff38d6407b3900ce5dc94634af119cb727e34f
AlexJang88 <77918244+AlexJang88@users.noreply.github.com>
2024-01-03 07:04:11 +0900

    Update CustomUserDetailsService.java

--- src/main/java/com/gogi/meatyou/security/CustomUserDetailsService.java
+++ src/main/java/com/gogi/meatyou/security/CustomUserDetailsService.java
@@ -1,20 +1,21 @@
 package com.gogi.meatyou.security;
 
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-import com.gogi.meatyou.bean.MemStatusDTO;
 import com.gogi.meatyou.bean.MemberDTO;
-import com.gogi.meatyou.repository.MemberMapper;
+import com.gogi.meatyou.repository.AdminMapper;
 
-
-public  class CustomUserDetailsService implements UserDetailsService{
-	//1226도준 영역
-		
-		@Autowired
+@Service
+public class CustomUserDetailsService implements UserDetailsService{
+	
+	@Autowired
 		private MemberMapper mapper;
 
 		MemStatusDTO mdto=new MemStatusDTO();
@@ -24,4 +25,27 @@ public  class CustomUserDetailsService implements UserDetailsService{
 
 			return dto == null ? null : new CustomUser(dto );
 		}
-	}
+	
+	
+	
+	
+	//@Autowired
+	//private AdminMapper mapper;
+	//@Override
+	//public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+	//	MemberDTO dto = mapper.read(username);
+	//	
+	//	return dto==null ? null:new CustomUser(dto);
+	//}
+
+	//@Autowired
+	//private SecurityMapper mapper;
+	
+	//@Override
+	//public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+	//		MemberDTO dto= mapper.read(username);
+			
+	//	return dto==null ? null:new CustomUser(dto);
+	//}
+	
+}
